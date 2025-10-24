458136ca6dee58f0bf76ca01a5b9828aaa9493fa
cvdante2020 <dante_emc@hotmail.com>
2025-10-23 22:36:28 -0500

    imagenes_amazon

--- next.config.ts
+++ next.config.ts
@@ -13,10 +13,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
+      { protocol: "https", hostname: "images-eu.ssl-images-amazon.com" },
+      
       // agrega otros dominios si usarás externos
     ],
   },
-  
+
 }
 
 
